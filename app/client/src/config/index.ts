b59684c5e63a11a0e481b2740957fe7184ae1d3a
xN8Tx <theeldern8t@ya.ru>
2024-10-18 00:30:29 +0700

    update data and config

--- app/client/src/config/index.ts
+++ app/client/src/config/index.ts
@@ -1,5 +1,5 @@
 export const EXTERNAL_URL = "https://shteyn-web.ru";
 export const LOCAL_URL = "http://localhost:3000";
-export const YANDEX_VERIFICATION = "0070624da8ef29a4";
+export const YANDEX_VERIFICATION = "4d7ec943e3b1d763";
 export const GOOGLE_VERIFICATION =
   "PcrVHW4cPTFeNnGowOE9Zi6kP8jWniWpiJgiB8MwV40";
